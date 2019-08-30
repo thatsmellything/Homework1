6bbd1e8c061b57d83203e5bacef641960b8b5373
thatsmellything <thesmelliestman@gmail.com>
2019-08-29 19:10:05 -0600

    Added MyDate with imports for the calendar

---
+++ src/controller/MyDate.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class MyDate 
+{
+	private int year;
+	private int month;
+	private int day;
+	
+	public MyDate()
+	{
+		
+	}
+
+}
