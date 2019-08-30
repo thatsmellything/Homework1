b830a68f41e7f2a2a0d54dc130d33bca2ad75e9c
thatsmellything <thesmelliestman@gmail.com>
2019-08-29 19:19:05 -0600

    Added MyDate setters and getters

--- src/controller/MyDate.java
+++ src/controller/MyDate.java
@@ -11,7 +11,44 @@ public class MyDate
 	
 	public MyDate()
 	{
+		GregorianCalendar cal = new GregorianCalendar();
+		year = cal.get(Calendar.YEAR);
+		month = cal.get(Calendar.MONTH);
+		day = cal.get(Calendar.DAY_OF_MONTH);
 		
+	}
+	
+	public MyDate(long elapsedTime) 
+	{
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTimeInMillis(elapsedTime);
+		year = cal.get(Calendar.YEAR);
+		month = cal.get(Calendar.MONTH);
+		day = cal.get(DAY_OF_YEAR);
+		
+		
+	}
+	
+	public MyDate(int year, int month, int day)
+	{
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	
+	public int getYear()
+	{
+		return year;
+	}
+	
+	public int getMonth()
+	{
+		return month;
+	}
+	
+	public int getDay()
+	{
+		return day;
 	}
 
 }
