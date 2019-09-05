08e4f241f6cb180b2ff176ca7b0a9d3493999d3b
thatsmellything <thesmelliestman@gmail.com>
2019-09-04 20:19:43 -0600

    Update Staff.java

--- src/model/Staff.java
+++ src/model/Staff.java
@@ -2,5 +2,22 @@ package model;
 
 public class Staff extends Employee
 {
+	Staff()
+	{
+		super();
+		System.out.println("this is in the grandchild class " + name);
+	}
+	
+	Staff(String N, String A, double P, String E)
+	{
+		super(N, A, P, E);
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Staff St = new Staff();
+		System.out.println("Name: " + St.name);
+	}
 
 }
