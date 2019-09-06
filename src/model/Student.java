f341e04d5c574a2458bb5e138a0a7f501d8aec09
thatsmellything <thesmelliestman@gmail.com>
2019-09-06 11:16:59 -0600

    Created and finished Student.java. Updated tester as needed

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,7 +1,27 @@
 package model;
 
-public class Student
+public class Student extends Person
 {
 	//school class freshman sophomore junior senior
 
+	String classStatus = "Freshman";
+	
+	public Student()
+	{
+		super("Jensen", "8328 Mountain View Towers", 801.6417630, "jensen.judkins@aggiemail.usu.edu");
+		
+		
+	}
+	
+	public Student(String N, String A, double P, String E)
+	{
+		super(N,A,P,E);
+	}
+	
+	public static void main(String[] args)
+	{
+		Student St = new Student("Jensen", "8328 Mountain View Towers", 801.6417630, "jensen.judkins@aggiemail.usu.edu");
+		System.out.println("Student Tester: " + St.getName() + ", " + St.getAddress() + ", " + St.getPhNumber() + ", " + St.getEMail() + ", " + St.classStatus);
+	}
+	
 }
--- src/tests/TestPerson.java
+++ src/tests/TestPerson.java
@@ -12,6 +12,8 @@ public class TestPerson
 	public static void main(String[] args)
 	{
 		Person.main(args);
+		Student.main(args);
+		Employee.main(args);
 		Staff.main(args);
 		
 	}
