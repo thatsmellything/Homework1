427db76d3693760b98691665a3b32b9c3cf129b4
thatsmellything <thesmelliestman@gmail.com>
2019-09-06 11:30:12 -0600

    Completed Faculty.java

--- src/model/Faculty.java
+++ src/model/Faculty.java
@@ -3,5 +3,25 @@ package model;
 public class Faculty extends Employee 
 {
 	//add office hours and a rank
+	
+	String officeHours = "9am-11:30am";
+	String rank = "Grand Wizard";
+	
+	Faculty()
+	{
+		super();
+		
+	}
+	
+	public Faculty(String N, String A, double P, String E)
+	{
+		super(N,A,P,E);
+	}
+	
+	public static void main(String[] args)
+	{
+		Faculty Fac = new Faculty("Faculty", "Faulty Work Address", 1800.20348720374, "Faculty@gmail.com");
+		System.out.println("Faculty Tester: " + Fac.getName() + ", " + Fac.getAddress() + ", " + Fac.getPhNumber() + ", " + Fac.getEMail() + ", " + Fac.officeHours + ", " + Fac.rank);
+	}
 
 }
